2eb96e463dd386deb7f39c3d0b14771b15701426
Tiarnan-Healy <146080824+Tiarnan-Healy@users.noreply.github.com>
2024-10-02 21:24:41 +0100

    Updated, menu loop,functioning

--- Calculator.java
+++ Calculator.java
@@ -10,25 +10,22 @@ public class Calculator {
         Scanner scanner = new Scanner(System.in);
         double num1 = 0;
         double num2 = 0;
-        double num3 = 0;
-        String choice = "";
 
         num1 = inputFirstNum(num1, scanner);
-        num2 = inputSecondNum(num2, scanner);
-        menu(scanner, num1, num2);
+        menu(scanner, num1);
     }
 
     // Input method
     public static double inputFirstNum(double num1, Scanner scanner) {
-        System.out.println("Enter the first number: ");
+        System.out.print("Enter the first number: ");
         num1 = scanner.nextDouble();
         return num1;
     }
 
-    public static double inputSecondNum(double num2, Scanner scanner) {
-        System.out.println("Enter the second number: ");
-        num2 = scanner.nextDouble();
-        return num2;
+    public static double inputSecondNum(Scanner scanner) {
+        System.out.print("Enter the second number: ");
+        return scanner.nextDouble();
+
     }
 
     // Addition method 
@@ -51,6 +48,57 @@ public class Calculator {
         return num1 * num2;
     }
 
+
+    // Square method
+    public static double square(double num1) {
+        return num1 * num1; }
+
+    //Cube method
+    public static double cube(double num1) {
+        return num1 * num1 * num1;
+    }
+
+    // Square root method
+    public static double squareRoot(double num1) {
+        num1 = Math.sqrt(num1);
+        return num1;
+    }
+
+    // Power method
+    public static double power(double num1, double num2) {
+        for (int i = 0; i < num2; i++) {
+            num1 = num1 * num1;
+        }
+        return num1;
+    }
+
+    // Factorial method
+    public static double factorial(double num1) {
+        double result = 1;
+        for (int i = 1; i <= num1; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    // Sine method
+    public static double sin(double num1) {
+        num1 = Math.sin(num1);
+        return num1;
+    }
+
+    // Cosine method
+    public static double cos(double num1) {
+        num1 = Math.cos(num1);
+        return num1;
+    }
+
+    // Tan method
+    public static double tan(double num1) {
+        num1 = Math.tan(num1);
+        return num1;
+    }
+
     // Quit method
     public static void quit() {
         System.out.println("Goodbye!");
@@ -58,36 +106,98 @@ public class Calculator {
     }
 
     // Print method
-    public static void print(double answer) {
-        System.out.println(answer);
+    public static void print(double answer, Scanner scanner) {
+        System.out.println("The result is: " + answer);
+        System.out.println("Press enter to continue...");
+        scanner.nextLine();
+        scanner.nextLine();
     }
 
     // Menu method
-    public static void menu(Scanner scanner, double num1, double num2){
+    public static void menu(Scanner scanner, double num1){
+        double answer = num1;
         while (true) {
+
             System.out.println("Please select an option:\n");
-            System.out.println("Addition\t\t\t(1)");
-            System.out.println("Subtraction\t\t\t(2)");
-            System.out.println("Multiplication\t\t(3)");
-            System.out.println("Division\t\t\t(4)");
-            System.out.println("Quit\t\t\t\t(5)");
-            int choice = scanner.nextInt();
+            System.out.println("Addition(1)\t\t\tPower(7)");
+            System.out.println("Subtraction(2)\t\tFactorial(8)");
+            System.out.println("Multiplication(3)\tSquare Root(9)");
+            System.out.println("Division(4)\t\t\tSin(10)");
+            System.out.println("Square(5)\t\t\tCos(11)");
+            System.out.println("Cube(6)\t\t\t\tTan(12)");
+            System.out.println("Reset(r)");
+            System.out.println("\nQuit(0)\n");
+            System.out.println(answer + "\n");
+            System.out.print("Enter your choice: ");
+            String choice = scanner.next();
+
             switch (choice) {
-                case 1:
-                    print(addition(num1, num2));
+                case "1": // Addition
+                    double num2 = inputSecondNum(scanner);
+                    answer = addition(answer, num2);
+                    print(answer, scanner);
                     break;
-                case 2:
-                    print(subtraction(num1, num2));
+                case "2": // Subtraction
+                    num2 = inputSecondNum(scanner);
+                    answer = subtraction(answer, num2);
+                    print(answer, scanner);
                     break;
-                case 3:
-                    print(multiplication(num1, num2));
+                case "3": // Multiplication
+                    num2 = inputSecondNum(scanner);
+                    answer = multiplication(answer, num2);
+                    print(answer, scanner);
                     break;
-                case 4:
-                    print(division(num1, num2));
+                case "4": // Division
+                    num2 = inputSecondNum(scanner);
+                    if (num2 != 0) {
+                        answer = division(answer, num2);
+                    } else {
+                        System.out.println("Error: Cannot divide by zero!");
+                    }
+                    print(answer, scanner);
                     break;
-                case 5:
+                case "5": // Square
+                    answer = square(answer);
+                    print(answer, scanner);
+                    break;
+                case "6": // Cube
+                    answer = cube(answer);
+                    print(answer, scanner);
+                    break;
+                case "7": // Power of own choosing
+                    num2 = inputSecondNum(scanner);
+                    answer = power(answer, num2);
+                    print(answer, scanner);
+                    break;
+                case "8": // Factorial
+                    answer = factorial(answer);
+                    print(answer, scanner);
+                    break;
+                case "9": // Square root
+                    answer = squareRoot(answer);
+                    print(answer, scanner);
+                    break;
+                case "10": // Sine
+                    answer = sin(answer);
+                    print(answer, scanner);
+                    break;
+                case "11": // Cosine
+                    answer = cos(answer);
+                    print(answer, scanner);
+                    break;
+                case "12": // Tan
+                    answer = tan(answer);
+                    print(answer, scanner);
+                    break;
+                case "r": // Reset
+                    num1 = inputFirstNum(0, scanner);  // Reset with new num1
+                    answer = num1;
+                    break;
+                case "0": // Quit
                     quit();
                     System.exit(0);
+                default:
+                    System.out.println("\nInvalid option, please input again.\n");
             }
         }
     }
